e67fc1c23959760c910ae73958c6781787aec7b0
Yash  Vadoliya <104588907+yash-vadoliya@users.noreply.github.com>
2024-03-29 08:15:27 +0530

    Add files via upload

---
+++ Resume/src/Resume.js
@@ -0,0 +1,22 @@
+import "./Resume.css";
+function Resume()
+{
+    return (
+        <div>
+            <h1>
+                ISHAAN AGRAWAL
+            </h1>
+            <div class="side">
+                <div class="contact">
+                              
+                </div>
+                <div class="data">
+                                      
+                </div>  
+            </div>
+        </div>
+
+    );
+}
+
+export default Resume;
