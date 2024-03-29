55d71cd0ae17db8eb91784da02441b257fc77b1f
Yash  Vadoliya <104588907+yash-vadoliya@users.noreply.github.com>
2024-03-29 09:12:43 +0530

    Add files via upload

--- Resume/src/Resume.js
+++ Resume/src/Resume.js
@@ -8,7 +8,26 @@ function Resume()
             </h1>
             <div class="side">
                 <div class="contact">
-                              
+                    <div >
+                        <h4 style= {{ paddingBottom:"0px",paddingTop:"0px"  }}>CONTACT</h4>
+                        <p style= {{ paddingBottom:"0px", paddingTop:"0px" }}>New Delhi,India 110034</p>
+                        <p style= {{ paddingBottom:"0px", paddingTop:"0px" }}>+91 22 5555 33</p>  
+                        <p style= {{ paddingBottom:"0px",paddingTop:"0px"  }}>i.agraval@example.in</p> 
+                    </div>  
+                    <hr style={{ margin:"12px", padding:"0px",}}></hr>
+                    <div>
+                        <h4 style= {{ paddingBottom:"0px", paddingLeft:"5px" }}>CORE QUALIFICTAIONS</h4>
+                        <ul style= {{ paddingBottom:"0px", }}>
+                            <li style= {{ paddingBottom:"0px",paddingTop:"0px", paddingLeft:"5px"  }}>Cashregister operation</li>
+                            <li style= {{ paddingBottom:"0px",paddingTop:"0px", paddingLeft:"5px"  }}>POS Syaytem operator</li>
+                            <li style= {{ paddingBottom:"0px",paddingTop:"0px", paddingLeft:"5px"  }}>Salseexpertise</li>
+                            <li style= {{ paddingBottom:"0px",paddingTop:"0px", paddingLeft:"5px"  }}>Teamwork</li>
+                            <li style= {{ paddingBottom:"0px",paddingTop:"0px" , paddingLeft:"5px" }}>Inventory management</li>
+                            <li style= {{ paddingBottom:"0px", paddingTop:"0px", paddingLeft:"5px" }}>Accurate money handling</li>
+                            <li style= {{ paddingBottom:"0px", paddingTop:"0px" , paddingLeft:"5px"}}>Documentation & Record-keeping</li>
+                            <li style= {{ paddingBottom:"0px",paddingTop:"0px" , paddingLeft:"5px" }}>Reatl Machandising Expertise</li>
+                        </ul>
+                    </div>
                 </div>
                 <div class="data">
                                       
